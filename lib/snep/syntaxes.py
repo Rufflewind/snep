0d81eaa0891d4b4d7794e88718bf7913f3c1b9c8
Phil Ruffwind <rf@rufflewind.com>
2016-07-06 20:20:35 -0400

    Delete old versions of snep
    (Syntax support is not added yet)

---
+++ lib/snep/syntaxes.py
@@ -0,0 +1,24 @@
+import re
+
+syntaxes = {
+  "c": [("/\*@", "\*/\s*\n")],
+  "c++": [("//@", "\n")],
+  "hs": [("--\s@", "\n"), ("{-@", "-}")],
+  "sh": [("#@", "\n")],
+}
+
+def guess_syntax(extension, shebang):
+
+    if extension in ["c", "cc", "cpp", "cxx", "c++", "C",
+                     "h", "hh", "hpp", "hxx", "h++", "H"]:
+        return ["c", "c++"]
+
+    if extension in ["hs", "hsc"]:
+        return ["hs"]
+
+    if re.search(r"^(py|\w*sh)$", extension):
+        return ["sh"]
+
+    if (re.search(r"/\w*sh\s", shebang) or
+        re.search(r"/i?python[.\d]*\s", shebang)):
+        return ["sh"]
